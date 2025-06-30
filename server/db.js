193291d451b37f744daf3d7a09c0fc6e0361e5d2
ZainM789 <zainuridarulmusthofa@gmail.com>
2025-06-30 11:37:51 -0400

    MonggoDB setup

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:<A28vhrJs9nOyAReYAdrO1aKu>@172.21.93.248:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
